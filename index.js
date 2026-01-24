3ebead5b5f8bc00d684007030dd508011ca73e9d
eeswari-s <eeswarivaithee@gmail.com>
2026-01-24 15:08:49 +0530

    c

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ connectDatabase();
 // ==============================
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
